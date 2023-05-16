2298abbc7dd0d328efa094cfd7a56c357b132e53
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-05-16 09:21:07 -0300

    chore: eslint install + config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/', () => {
   return 'hello world'
-});
-
-app.listen({
-  port: 3333
 })
-.then(() => console.log('🏃‍♂️ server running at: http://localhost:3333'));
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('🏃‍♂️ server running at: http://localhost:3333'))
