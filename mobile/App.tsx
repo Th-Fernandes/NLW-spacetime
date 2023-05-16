ec1eb9f95461b608d749760db0b81a14111ff155
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-05-16 14:38:36 -0300

    chore: tailwind prettier install + config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className='bg-slate-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 text-4xl font-bold'>NLW together</Text>
+    <View className="flex-1 items-center justify-center bg-slate-950">
+      <Text className="text-4xl font-bold text-zinc-50">NLW together</Text>
       <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
