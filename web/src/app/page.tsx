b0696aceebe7e4c361f420b5335b72c97731fba2
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-05-16 13:34:03 -0300

    feat: eslint config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <div className="h-screen bg-zinc-800 text-zinc-50">
+      <h1 className="text-4xl font-bold">hello world</h1>
+    </div>
   )
 }
